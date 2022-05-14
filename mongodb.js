37c1e3ec58f105dc0757ae0ba78d0a83243ba66f
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-14 14:32:15 +0200

    Update how to read and update documents in mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,15 @@
 //CRUD - create read update delete operations
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { mongodb, MongoClient, ObjectId } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+//to access ObjectId and its properties
+/* const id = new ObjectId(); 
+console.log(id);
+console.log(id.getTimestamp());
+console.log(id.toHexString()); */
 
 MongoClient.connect(
   connectionURL,
@@ -19,8 +24,9 @@ MongoClient.connect(
     //we inserted our first document
     /* db.collection("users").insertOne(
       {
-        name: "Slobodan",
-        age: "30",
+        _id: id, //optional, if we want to set up id, but its extra code, because mongodb auto generates it
+        name: "Vikram",
+        age: 35,
       },
       (error, result) => {
         if (error) {
@@ -47,6 +53,82 @@ MongoClient.connect(
       }
     ); */
 
-    
+    //to find a single entry, can be name or ObjectId or any other property
+    /* db.collection("users").findOne(
+      { _id: new ObjectId("627f7937f71e74f67a6cf4c0") },
+      (error, user) => {
+        if (error) {
+          return console.log("Unable to fetch");
+        }
+
+        console.log(user);
+      }
+    ); */
+
+    //search for multiple users
+    /* db.collection("users")
+      .find({ age: 27 })
+      .toArray((error, users) => {
+        console.log(users);
+      }); */
+
+    //UPDATE ENTRY, change name on item with this ObjectId
+    db.collection("users")
+      .updateOne(
+        {
+          _id: new ObjectId("627f838ad51708c7ed6ff5e2"),
+        },
+        {
+          //https://www.mongodb.com/docs/manual/reference/operator/update/ Mongodb update operators
+          $set: {
+            name: "John",
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    //INCREMENT age by 1
+    db.collection("users")
+      .updateOne(
+        {
+          _id: new ObjectId("627f838ad51708c7ed6ff5e2"),
+        },
+        {
+          //https://www.mongodb.com/docs/manual/reference/operator/update/ Mongodb update operators
+          $inc: {
+            age: 1,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    //UPDATE MANY items
+    db.collection("tasks")
+      .updateMany(
+        {
+          completed: true,
+        },
+        {
+          $set: {
+            completed: false,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 );
