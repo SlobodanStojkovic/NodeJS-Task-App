c424e078d56cb44f8d44b8376c8b36fd7d5d53ca
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-14 12:11:34 +0200

    Update how to insert single and multiple documents into mongodb

--- mongodb.js
+++ mongodb.js
@@ -11,9 +11,42 @@ MongoClient.connect(
   { useNewUrlParser: true },
   (error, client) => {
     if (error) {
-      console.log("Unable to connect to database");
+      return console.log("Unable to connect to database");
     }
 
-    console.log("Connected correctly!");
+    const db = client.db(databaseName);
+
+    //we inserted our first document
+    /* db.collection("users").insertOne(
+      {
+        name: "Slobodan",
+        age: "30",
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user");
+        }
+
+        console.log(result);
+      }
+    ); */
+
+    //create multiple documents
+    /* db.collection("tasks").insertMany(
+      [
+        { description: "Learn course", completed: true },
+        { description: "Pay bills", completed: true },
+        { description: "Buy food", completed: false },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log(error);
+        }
+
+        console.log(result);
+      }
+    ); */
+
+    
   }
 );
