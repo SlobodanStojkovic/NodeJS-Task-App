de89640c92aa8dfdc9aa67229132c5c4af8e8f9b
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-15 19:52:29 +0200

    Add Resource updating and deleting endpoints

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+//CREATE USER
 app.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
@@ -18,6 +19,7 @@ app.post("/users", async (req, res) => {
   }
 });
 
+//READ USERS
 app.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
@@ -27,6 +29,7 @@ app.get("/users", async (req, res) => {
   }
 });
 
+//READ USER
 app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
 
@@ -43,6 +46,52 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+//UPDATE USER, to update someone in body we need to send json data with {"name": "Updated name value"}
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+
+  //every returns true if 4/4 values are true, if 1 is not true it will return false
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+  //new to return new updated value, runValidators to do validations for updated values
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//DELETE USER
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//CREATE TASKS
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
@@ -54,6 +103,7 @@ app.post("/tasks", async (req, res) => {
   }
 });
 
+//READ TASKS
 app.get("/tasks", async (req, res) => {
   try {
     const tasks = await Task.find({});
@@ -63,6 +113,7 @@ app.get("/tasks", async (req, res) => {
   }
 });
 
+//READ TASK
 app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
 
@@ -71,6 +122,49 @@ app.get("/tasks/:id", async (req, res) => {
 
     if (!task) {
       return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//UPDATE TASK
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body); //returns array of properties
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
+//DELETE TASK
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      res.status(404).send();
     }
 
     res.send(task);
