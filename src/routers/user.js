d7a10ebfbd3b2adfb686557743e2e72ed33e2e1f
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-17 14:00:57 +0200

    Update Actions now require user to be logged in, Add logout and Logout all sessions

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
-//CREATE USER
+//CREATE USER - SIGN UP
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error); //chaining, this is same as res.status(400) res.send(e)
   }
@@ -20,20 +22,40 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (error) {
     res.status(400).send();
   }
 });
 
-//READ USERS
-router.get("/users", async (req, res) => {
+//LOGOUT USER
+router.post("/users/logout", auth, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
   } catch (error) {
     res.status(500).send();
   }
+});
+
+//LOGOUT ALL SESSIONS
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+//READ OUR OWN USER PROFILE
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 //READ USER
