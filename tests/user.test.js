fc73132986cc93bbd0a4ba1ea45528d369de806b
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-25 15:54:47 +0200

    Add Advanced assertions in tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,29 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+  _id: userOneId,
+  name: "Mike",
+  email: "mike@example.com",
+  password: "56what!!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
+};
+
 beforeEach(async () => {
   await User.deleteMany();
+  await new User(userOne).save();
 });
 
 test("Should signup a new user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/users")
     .send({
       name: "TestSlobodan",
@@ -15,4 +31,66 @@ test("Should signup a new user", async () => {
       password: "MyPass777!",
     })
     .expect(201);
+
+  // asert that the database was changed correctly
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  // Assertions about the response
+  expect(response.body).toMatchObject({
+    user: {
+      name: "TestSlobodan",
+      email: "test@gmail.com",
+    },
+    token: user.tokens[0].token,
+  });
+  expect(user.password).not.toBe("MyPass777!"); //to check that plain test password is not store inside database
+});
+
+test("Should login existing user", async () => {
+  const response = await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+  const user = await User.findById(userOneId);
+  expect(response.body.token).toBe(user.tokens[1].token);
+});
+
+test("Should not login nonexisting user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "wrongemail@example.com",
+      password: "wrongpassword",
+    })
+    .expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not get profile for unauthenticated user", async () => {
+  await request(app).get("/users/me").send().expect(401);
+});
+
+test("Should delete account for user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+  const user = await User.findById(userOneId);
+  expect(user).toBeNull();
+});
+
+test("should not delete account for unauthenticate user", async () => {
+  await request(app).delete("/users/me").send().expect(401);
 });
